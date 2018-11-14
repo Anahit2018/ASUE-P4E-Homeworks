50bc37a5f40e1a943f823e32fc1ec6d9f9b4a69e
Anahit2018 <anahit.khach@gmail.com>
2018-11-14 14:02:22 +0330

    HW3 review

--- Homework3/problem2.3.py
+++ Homework3/problem2.3.py
@@ -1,18 +1,13 @@
-#SOLUTION APPROACH
-#1. Form a range between 0 and inputed number
-#2. Take each number from range and divide by its predecessors.If divisable by any (num%i==0) not a prime, take it out. Prime divisible to only 1(which we exclude in function) and itself.
-#3. Print whatever stayed (primes)
-
 random_num = int(input("Input an integer: "))
+random_list = list(range(0, random_num+1))
+#print(random_list)
 
-print("Prime numbers between 0 and", random_num,"are:")
-
-for num in range(0, random_num+ 1):
-   # prime numbers are greater than 1
-   if num > 1:
-       for i in range(2,num):
-           if (num % i) == 0:
-               break
+def find_prime(random_list):
+    for num in random_list:
+       # prime numbers are greater than 1
+       if num > 1:
+           for i in range(2,num):
+               if (num % i) == 0:
+                   break
        else:
-           print(num, end=' ')
-print('\n')
+           print(num)
