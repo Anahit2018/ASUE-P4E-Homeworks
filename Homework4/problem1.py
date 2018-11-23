034b9f1e6a465f2e6d3e2e8cee72f3ee32de1a1d
Anahit2018 <anahit.khach@gmail.com>
2018-11-23 12:03:57 +0330

    code reviewed according to PEP8

--- Homework4/problem1.py
+++ Homework4/problem1.py
@@ -1,56 +1,57 @@
 import random
-#import sys
-count_wins=0
-count_loses=0
-count_switch=0
-count_iteration=0
-count_loses_by_switch=0
-count_wins_by_switch=0
+count_wins = 0
+count_loses = 0
+count_switch = 0
+count_iteration = 0
+count_loses_by_switch = 0
+count_wins_by_switch = 0
 
-n=int(input("How many times would you like to play? "))
+n = int(input("How many times would you like to play? "))
+print("\n")
+while n != count_iteration:
+    x = random.randint(1, 3)  # x=contestant
+    y = random.randint(1, 3)  # y=prize
+    z = random.randint(1, 3)  # z=host
+    while z == x or z == y:
+        z = random.randint(1, 3)
+    x_c = random.randint(1, 3)
+    while x_c == z:
+        x_c = random.randint(1, 3)
 
-while n !=count_iteration:
-  x=random.randint(1,3) # x=contestant
-  y=random.randint(1,3) #y=prize
-  #print("Prize is behind:",y)
-  #if x==y:
-      #print("YOU WON!")
-     # sys.exit()  # option to stop game if u guess door at once
-  
-  z=random.randint(1,3) #z=host
-  while z==x or z==y:
-    z=random.randint(1,3)
-  #print("Host opened:",z)
- 
-  #print("Do you still want to open door",x,end=' ')
-  x_c=random.randint(1,3)
-  while x_c==z:
-    x_c=random.randint(1,3)
-  if x_c==y:
-    count_wins +=1
-    count_iteration +=1
-    if x_c!=x:
-      count_switch +=1
-      count_wins_by_switch +=1
-      print("Prize is behind door",y)
-      print("Contestant chooses door",x)
-      print("Host opens door", z)
-      print("Contestant switches from door",x,"to door",x_c)
-      print("Contestant Won!\n")
-    #print("YOU WON!!! Prize WAS behind door number",y, "\n")
-#if x_c==z:
-    #print("Door", z, "is already open.")
-  if x_c!=y:
-    count_loses +=1
-    count_iteration +=1
-    if x_c==x:
-      count_loses_by_switch +=1
-      print("Prize is behind door",y)
-      print("Contestant chooses door",x)
-      print("Host opens door", z)
-      print("Contestant doesn't swtich the door")
-      print("Contestant Lost!\n")
-    #print("You lost\n")
+    if x_c == y:
+        count_wins += 1
+        count_iteration += 1
+        if x_c != x:
+            count_switch += 1
+            count_wins_by_switch += 1
+            print("Prize is behind door", y)
+            print("Contestant chooses door", x)
+            print("Host opens door", z)
+            print("Contestant switches from door", x, "to door", x_c)
+            print("Contestant Won!\n")
+        else:
+            print("Prize is behind door", y)
+            print("Contestant chooses door", x)
+            print("Host opens door", z)
+            print("Contestant doesn't switch the door")
+            print("Contestant Won!\n")
+
+    if x_c != y:
+        count_loses += 1
+        count_iteration += 1
+        if x_c == x:
+            count_loses_by_switch += 1
+            print("Prize is behind door", y)
+            print("Contestant chooses door", x)
+            print("Host opens door", z)
+            print("Contestant doesn't swtich the door")
+            print("Contestant Lost!\n")
+        else:
+            print("Prize is behind door", y)
+            print("Contestant chooses door", x)
+            print("Host opens door", z)
+            print("Contestant switches from door", x, "to door", x_c)
+            print("Contestant Lost!\n")
 
 print("Number of iterations:", count_iteration)
 print('Number of wins:', count_wins)
@@ -60,5 +61,7 @@ print("Number of loses due to switching:", count_loses_by_switch)
 print("Number of wins due to switching:", count_wins_by_switch)
 prob_of_win_by_switching = count_wins_by_switch/count_wins*100
 prob_of_not_win_by_switching = 100-prob_of_win_by_switching
-print("Probability of winning when switching:",prob_of_win_by_switching, "%")
-print("Probability of winning when NOT switching:",prob_of_not_win_by_switching,"%")
+print("Probability of winning when switching:",
+      (round((prob_of_win_by_switching), 2)), "%")
+print("Probability of winning when NOT switching:",
+      (round((prob_of_not_win_by_switching), 2)), "%")
